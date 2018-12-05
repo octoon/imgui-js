d9e431934035bb21cff305d022f3a1a90292296b
JasonWang <1103870790@qq.com>
2018-12-05 12:17:45 +0800

    update

--- src/imgui_stdweb.rs
+++ src/imgui_stdweb.rs
@@ -1,5 +1,31 @@
 
 
+#[derive(Debug, Clone)]
+pub struct ImguiIO{
+}
+
+#[derive(Debug, Clone)]
+pub struct ImDrawList{
+    draw_list:stdweb::Value
+}
+
+impl ImDrawList{
+    pub fn vtx_buffer(&self) -> &stdweb::Value{
+        self.draw_list.VtxBuffer
+    }
+
+    pub fn idx_buffer(&self) -> &stdweb::Value{
+        self.draw_list.IdxBuffer
+    }
+
+    pub fn flags(&self) -> &stdweb::Value{
+        self.draw_list.Flags
+    }
+
+    pub fn iterate_draw_lists(&self){
+        
+    }
+}
 
 #[derive(Debug, Clone)]
 pub struct Imgui{
@@ -35,5 +61,20 @@ impl Imgui{
             return @{module}.get_io();
         };
         ret
+    }
+
+    pub fn style_colors_dark(&self){
+        let module = &self.module;
+        js! {
+            return @{module}.StyleColorsDark();
+        };
+    }
+
+    #[allow(non_snake_case)]
+    pub fn IM_ASSERT(&self, cond:bool){
+        let module = &self.module;
+        js! {
+            @{module}.IM_ASSERT(@{cond});
+        };
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 #[cfg(all(target_arch = "wasm32"))]
 extern crate stdweb;
 
+#[cfg(not(target_arch = "wasm32"))]
+extern crate imgui_sys;
 
 #[cfg(all(target_arch = "wasm32"))]
 pub mod imgui_stdweb;
